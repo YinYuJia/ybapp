ec2dfc35650adbb48c2b2cd7826b782552ecf201
zzxprint <zzxprint@163.com>
2019-04-15 16:39:17 +0800

    办事进度组件

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -11,6 +11,7 @@
     <!-- 菜单栏 -->
     <div class="menu_bar">
         <MenuBar></MenuBar>
+        <WorkProgress></WorkProgress>
     </div>
     <!-- 轮播图 -->
     <div class="Rotation_chart">
@@ -23,6 +24,7 @@
 <script>
   import Header from './commonComponent/Index/Header'
   import Navbar from './commonComponent/Index/Navbar'
+  import WorkProgress from './commonComponent/WorkProgress'
   import MenuBar from './commonComponent/IconNav'
   import RotationChart from './lunbotu/RotationChart'
   import Footer from './Footer.vue'
@@ -48,7 +50,8 @@
       'Navbar': Navbar,
       'MenuBar': MenuBar,
       'RotationChart': RotationChart,
-      "Footer": Footer
+      "Footer": Footer,
+      "WorkProgress": WorkProgress
     },
     created() {
        console.log(MenuBar)
---
+++ src/components/commonComponent/WorkProgress.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="progress">
+        <div class="progress-btn">办事进度</div>
+        <span class="progress-msg">{{msg}}</span>
+        <embed src="../../../static/images/index/WorkPro_more.svg" type="">
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return{
+            msg: '异地就医已办理完成'
+        }
+    }
+}
+</script>
+
+<style lang="less" scoped>
+ .progress{
+    position: relative;
+    height: .91rem;
+    display: flex;
+    align-items: center;
+    background: #fff;
+    &:before{
+        content: '';
+        position: absolute;
+        top: 0;
+        left: 50%;
+        margin-left: -6.54rem/2;
+        height: .02rem;
+        width: 6.54rem;
+        background-color: #E3E3E3;
+    }
+    
+    .progress-btn{
+        height: .4rem;
+        width: 1.2rem;
+        margin-left: .53rem;
+        border-radius: .06rem;
+        background: #519AFF;
+        line-height: .4rem;
+        font-family: PingFangSC-Regular;
+        font-size: .22rem;
+        color: #FFFFFF;
+        letter-spacing: 0;
+    }
+    .progress-msg{
+        margin-left: .26rem;
+        opacity: 0.65;
+        font-family: PingFangSC-Regular;
+        font-size: .24rem;
+        color: #000000;
+        letter-spacing: 0;
+    }
+    embed{
+        display: block;
+        height: .3rem;
+        width: .3rem;
+        margin-left: 2.71rem;
+    }
+ }
+</style>
