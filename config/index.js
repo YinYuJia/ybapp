8f99fce869a6185fc19b7edc6f97fa503328e639
yinyujia <6451584@qq.com>
2019-04-25 10:35:34 +0800

    自动打开浏览器

--- config/index.js
+++ config/index.js
@@ -17,7 +17,7 @@ module.exports = {
     host: '192.168.1.169', 
  
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
-    autoOpenBrowser: false,
+    autoOpenBrowser: true,
     errorOverlay: true,
     notifyOnErrors: true,
     poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
