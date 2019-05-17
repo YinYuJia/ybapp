79fae8851de73cf4dad317fc42e42efe3172c8c0
zzxprint <zzxprint@163.com>
2019-05-17 15:51:06 +0800

    重新配置

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Index from '@/components/Index'
-import First from '@/components/First';
-import Work from '@/components/Work';
-import TreatmentReimbursement from '@/components/commonComponent/Work/TreatmentReimbursement';
-import TReimbursement1 from '@/components/commonComponent/Work/TreatmentReimbursement/TReimbursement1'
-import SubmitReimbursement from '@/components/commonComponent/Work/TreatmentReimbursement/SubmitReimbursement'
-import EventDetail from '@/components/commonComponent/Work/TreatmentReimbursement/EventDetail_1'
+import Index from '@/components/serveComponent/index'
+// import First from '@/components/First';
+// import Work from '@/components/Work';
+// import TreatmentReimbursement from '@/components/commonComponent/Work/TreatmentReimbursement';
+// import TReimbursement1 from '@/components/commonComponent/Work/TreatmentReimbursement/TReimbursement1'
+// import SubmitReimbursement from '@/components/commonComponent/Work/TreatmentReimbursement/SubmitReimbursement'
+// import EventDetail from '@/components/commonComponent/Work/TreatmentReimbursement/EventDetail_1'
 
 Vue.use(Router)
 
@@ -19,36 +19,36 @@ export default new Router({
       component: Index
     },
 
-    {
-      path: '/first',
-      name: 'FirstComponent',
-      component: First
-    },
-    // 办事
-    {
-      path: '/Work',
-      name: 'Work',
-      component: Work
-    },
-    {
-      path: '/Work/TreatmentReimbursement',
-      name: 'TreatmentReimbursement',
-      component: TreatmentReimbursement
-    },
-    {
-      path: '/Work/TreatmentReimbursement/TReimbursement1',
-      name: 'TReimbursement1',
-      component: TReimbursement1
-    },
-    {
-      path: '/Work/TreatmentReimbursement/TReimbursement1/SubmitReimbursement',
-      name: 'SubmitReimbursement',
-      component: SubmitReimbursement
-    },
-    {
-      path: '/EventDetail',
-      name: 'EventDetail',
-      component: EventDetail
-    },
+    // {
+    //   path: '/first',
+    //   name: 'FirstComponent',
+    //   component: First
+    // },
+    // // 办事
+    // {
+    //   path: '/Work',
+    //   name: 'Work',
+    //   component: Work
+    // },
+    // {
+    //   path: '/Work/TreatmentReimbursement',
+    //   name: 'TreatmentReimbursement',
+    //   component: TreatmentReimbursement
+    // },
+    // {
+    //   path: '/Work/TreatmentReimbursement/TReimbursement1',
+    //   name: 'TReimbursement1',
+    //   component: TReimbursement1
+    // },
+    // {
+    //   path: '/Work/TreatmentReimbursement/TReimbursement1/SubmitReimbursement',
+    //   name: 'SubmitReimbursement',
+    //   component: SubmitReimbursement
+    // },
+    // {
+    //   path: '/EventDetail',
+    //   name: 'EventDetail',
+    //   component: EventDetail
+    // },
   ]
 })
