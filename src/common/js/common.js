d1685a211258ad05e80946df5386d7c89cc1511a
yinyujia <6451584@qq.com>
2019-06-11 12:40:34 +0800

    测试包

--- src/common/js/common.js
+++ src/common/js/common.js
@@ -10,23 +10,23 @@ export default {
     // 公用请求地址
     // 开发地址
     
-    ApiUrl:function() {
-        return 'http://192.168.1.199:13030' // 吴学文
-    },
-    
-    ApiUrl1:function() {
-        return 'http://192.168.1.96:13030' // 胡伟
-    },
-
-    ApiUrl2:function() {
-        return 'http://192.168.1.157:13030' // 魏陈涛
-    },
-    
-
-    // 打包测试地址
     // ApiUrl:function() {
-    //     return ''
+    //     return 'http://192.168.1.199:13030' // 吴学文
     // },
+    
+    // ApiUrl1:function() {
+    //     return 'http://192.168.1.96:13030' // 胡伟
+    // },
+
+    // ApiUrl2:function() {
+    //     return 'http://192.168.1.157:13030' // 魏陈涛
+    // },
+    
+
+    //打包测试地址
+    ApiUrl:function() {
+        return ''
+    },
     SaveNativeData( data ) {
       return data
     },
