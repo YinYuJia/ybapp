f63fed05e67518c528ce2d5fa210306999d31c16
zzxprint <zzxprint@163.com>
2019-06-16 15:58:53 +0800

    零星报销

--- src/components/serveComponent/treat/smallReim/smallReim.vue
+++ src/components/serveComponent/treat/smallReim/smallReim.vue
@@ -143,7 +143,7 @@ export default {
                 submitForm.AAE135 = "113344223344536624";
             }
             // 请求参数封装
-            const params = this.epFn.commonRequsetData(this.$store.state.SET_NATIVEMSG.PublicHeader,submitForm,1012);
+            const params = this.epFn.commonRequsetData(this.$store.state.SET_NATIVEMSG.PublicHeader,submitForm,1026);
             return params;
         }
     }
