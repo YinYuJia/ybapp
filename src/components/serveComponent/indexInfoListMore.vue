e0b9b58f77debef8b83cd868bd64bbf1c883d26c
zzxprint <zzxprint@163.com>
2019-06-19 18:51:09 +0800

    更多  页面

---
+++ src/components/serveComponent/indexInfoListMore.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="indexInfoListMore">
+        <div class="infoListHeader">
+            <div class="swiper-container">
+                <div class="swiper-wrapper">
+                    <div class="swiper-slide">参保服务</div>
+                    <div class="swiper-slide">备案服务</div>
+                    <div class="swiper-slide">待遇服务</div>
+                    <div class="swiper-slide">其他服务</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import Swiper from 'swiper';
+export default {
+    mounted(){
+        new Swiper('.swiper-container', {
+            slidesPerView: 1.5, //显示的范围
+            spaceBetween: -8, //间隔大小
+        })
+    },
+}
+</script>
+
+<style lang="less" scoped>
+.indexInfoListMore{
+    .infoListHeader{
+        height: .94rem;
+        width: 7.5rem;
+        background: #FFF;
+        .swiper-container{
+            height: 100%;
+            width: 100%;
+            .swiper-wrapper{
+                .swiper-slide{
+                    height: .94rem;
+                    width: 2rem;
+                    line-height: .94rem;
+                    font-size: .36rem;
+                    color: #333333;
+                    letter-spacing: -0.0064rem;
+                    text-align: center;
+                }
+            }
+        }
+    }
+}
+</style>
