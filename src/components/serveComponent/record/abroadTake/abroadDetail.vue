567a79ed91bbab8f288afd0755b0b38c10ce2ed2
unknown <huzh2@epsoft.com.cn>
2019-06-21 10:07:16 +0800

    1009请求成功修改

--- src/components/serveComponent/record/abroadTake/abroadDetail.vue
+++ src/components/serveComponent/record/abroadTake/abroadDetail.vue
@@ -79,8 +79,11 @@ export default {
                 console.log('返回成功信息',resData)
                 //   成功   1000
                 if ( resData.enCode == 1000 ) {
-                    // this.currentStep = Number(resData.LS_DS[0].BOD037)   
-                    this.$toast("提交成功");
+                    if (resData.LS_DS.length > 0 ) {
+                       this.currentStep = Number(resData.LS_DS[0].BOD037) 
+                    }else{
+                        this.$toast("暂无状态信息")
+                    }
                 }else if (resData.enCode == 1001 ) {
                 //   失败  1001
                     this.$toast(resData.msg);
@@ -97,9 +100,7 @@ export default {
                 console.log('返回成功信息',resData)
                 //   成功   1000
                 if ( resData.enCode == 1000 ) {  
-                    this.List=[...this.List,...resData.LS_DS_10]
-                    console.log('this.List',this.List)
-                    this.form={...this.form,...this.List[0]}
+                    this.form={...this.form,...resData.LS_DS_10}
                     this.handleNumber = resData.LS_DS_10.BKZ019
                     this.$toast("提交成功");
                 }else if (resData.enCode == 1001 ) {
