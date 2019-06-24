e1b33a3ccb3027a33e98d0df8abea920a961ad05
yinyujia <6451584@qq.com>
2019-06-24 17:09:32 +0800

    C:/Program Files/Git/api

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,7 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  baseURL : '"http://10.85.159.203:13030"',
+  // baseURL : '"http://10.85.159.203:13030"',
+  baseURL : '"/api"',
   ifDev:false
 }
