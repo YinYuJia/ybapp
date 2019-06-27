f7991b9c8d19d30cfe169ac2c2455c49e56a6ef2
yinyujia <6451584@qq.com>
2019-06-27 22:11:58 +0800

    1

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,7 +1,7 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  // baseURL : '"//59.202.42.147:23030"',  //浙理办打包
+  // baseURL : '"//ybj.zjzwfw.gov.cn/api"',  //浙理办打包
   baseURL : '"/api/api"',
   ifDev:false
 }
