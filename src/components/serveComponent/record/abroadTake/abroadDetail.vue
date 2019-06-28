f9158136493d6b067b0d377f1166121b2faa48dc
zzxprint <zzxprint@163.com>
2019-06-28 23:59:32 +0800

    更改

--- src/components/serveComponent/record/abroadTake/abroadDetail.vue
+++ src/components/serveComponent/record/abroadTake/abroadDetail.vue
@@ -162,8 +162,7 @@ export default {
                 submitForm.AAC003 = this.$store.state.SET_NATIVEMSG.name;
                 submitForm.AAE135 = this.$store.state.SET_NATIVEMSG.idCard;
             }else {
-                submitForm.AAC003 = '许肖军';
-                submitForm.AAE135 = "332625197501010910";
+                this.$$toast("未获取到人员基本信息")
             }
             
             // 请求参数封装
@@ -186,8 +185,7 @@ export default {
                 submitForm.AAC003 = this.$store.state.SET_NATIVEMSG.name;
                 submitForm.AAE135 = this.$store.state.SET_NATIVEMSG.idCard;
             }else {
-                submitForm.AAC003 = '许肖军';
-                submitForm.AAE135 = "332625197501010910";
+                this.$$toast("未获取到人员基本信息")
             }
             
             // 请求参数封装
