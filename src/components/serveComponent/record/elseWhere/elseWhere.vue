d004b06da00da42aab7e3fe8d29f8811b208f9d4
zzxprint <zzxprint@163.com>
2019-06-28 09:49:14 +0800

    关闭提交成功提示，调整成功页面样式

--- src/components/serveComponent/record/elseWhere/elseWhere.vue
+++ src/components/serveComponent/record/elseWhere/elseWhere.vue
@@ -278,7 +278,7 @@ export default {
                         console.log('返回成功信息',resData)
                         //   成功   1000
                             if ( resData.enCode == 1000 ) {
-                                this.$toast("提交成功");
+                                // this.$toast("提交成功");
                                 this.$router.push("/elseDetail");
                             }else if (resData.enCode == 1001 ) {
                             //   失败  1001
