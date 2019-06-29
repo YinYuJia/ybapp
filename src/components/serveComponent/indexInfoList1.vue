6fd8159d1105b82e6e26fdd831c4c7970f294664
yinyujia <6451584@qq.com>
2019-06-29 22:30:51 +0800

    1

--- src/components/serveComponent/indexInfoList1.vue
+++ src/components/serveComponent/indexInfoList1.vue
@@ -427,8 +427,11 @@
                 }
 
                 if ( url == "smallReim" ) {
-                    this.$toast(tipstr + "服务暂未开通")
-                    return;
+                    if ( tip != "339900" ) {
+                        this.$toast(tipstr + "服务暂未开通")
+                        return
+                    }
+
                 }
                 let str = sessionStorage.getItem("GinsengLandCode")
                 console.log('str', str)
