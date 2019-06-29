88bdd3a0419fa628956421d286a5cc527014e163
unknown <huzh2@epsoft.com.cn>
2019-06-29 22:13:28 +0800

    特治特药AGA002参数修改

--- src/components/serveComponent/record/specialDrug/specialDrugDetail.vue
+++ src/components/serveComponent/record/specialDrug/specialDrugDetail.vue
@@ -174,7 +174,7 @@ export default {
         formatSubmitData(){
             let submitForm = {}
             console.log(submitForm)
-                submitForm.AGA002 =  "330800253023";
+                submitForm.AGA002 =  "330800253003";
                 // submitForm.debugTest=  "true";
 
             // 加入用户名和电子社保卡号
