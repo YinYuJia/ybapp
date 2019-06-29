ba8c9782a87c4c3dd3f783a4179942be69a80cf2
zzxprint <zzxprint@163.com>
2019-06-29 15:08:31 +0800

    查看大图

---
+++ src/components/serveComponent/common/PhotoView.vue
@@ -0,0 +1,47 @@
+<template>
+    <div v-if="show" class="PhotoView" @click="closePhoto">
+        <img :src="imgUrl">
+    </div>
+</template>
+
+<script>
+export default {
+    props:{
+        imgUrl:{
+            type: String,
+            default: ''
+        }
+    },
+    data(){
+        return{
+            show: false,
+        }
+    },
+    methods:{
+        open(){
+            this.show = true;
+        },
+        closePhoto(){
+            this.show = false;
+        },
+    }
+}
+</script>
+
+<style lang="less" scoped>
+.PhotoView{
+    position: absolute;
+    z-index: 998;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    display: flex;
+    align-items: center;
+    background: #0e0e0e;
+    img{
+        -webkit-user-select: none;
+        max-width: 100%;
+    }
+}
+</style>
